52cf6da69bd470b6e5888ea44ff73b3e4d0ae072
SagWel <nicolasbensault@free.fr>
2025-10-15 21:06:35 +0200

    up

--- D10H_!/src/composants/FavorisMain.jsx
+++ D10H_!/src/composants/FavorisMain.jsx
@@ -3,10 +3,11 @@ import { Box, Flex, Heading, Button} from "@chakra-ui/react";
 function main () {
     return (
         <Box id="main"
-        overflowY={"auto"} height={"100%"}>
+        overflowY={"auto"} height={"100%"}
+        margin={"0 3.0625rem"} padding={"1.5rem 1.5rem 0 1.5rem"}>
             <Box id="header-container">
-                <Box>
-                    <Heading as={"h2"}>Favoris</Heading>
+                <Box color={"#fdfcfe"}>
+                    <Heading as={"h2"} fontSize={"64px"} fontFamily={"Tahoma,Arial,sans-serif"}>Favoris</Heading>
                     
                 </Box>
             </Box>
